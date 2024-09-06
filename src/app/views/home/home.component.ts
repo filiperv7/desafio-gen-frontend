b6a745674e703a3c0cd4ac2decb7e401be0b9898
Filipe Rodrigo <filipevasconcelos91.rodrigo@gmail.com>
2024-09-06 18:00:50 -0300

    ♻️ refactor container div and button-primary style

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ContainerComponent } from '../../components/container/container.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, ContainerComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
